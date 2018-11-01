835cc4fa13b4c6f964745acac60a3f16d5d0b807
Jarod Burt <sjburt79@gmail.com>
2018-11-01 19:45:28 -0400

    Added JS
    Added JS Function and cleaned up CSS

---
+++ Final_Project/js/Game.js
@@ -0,0 +1,12 @@
+// 
+//Mouseover Events
+//
+
+$(document).ready(function(){
+    $("h1").mouseover(function(){
+        $("h1").css("background-color", "yellow");
+    });
+    $("h1").mouseout(function(){
+        $("h1").css("background-color", "lightgray");
+    });
+});
