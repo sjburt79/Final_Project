687bec8759cb7c656f2d3e5073fe307afbd34127
Jarod Burt <sjburt79@gmail.com>
2018-11-08 19:50:15 -0500

    Final touches
    worked on JS functions

--- Final_Project/js/Game.js
+++ Final_Project/js/Game.js
@@ -10,3 +10,19 @@ $(document).ready(function(){
         $("h1").css("background-color", "lightgray");
     });
 });
+
+/* document.addEventListener("mouseover", function(){
+    document.getElementById("gameone").style.background = "red";
+});
+
+document.addEventListener("mouseout", function(){
+    document.getElementById("gameone").style.background = "white";
+});
+
+document.addEventListener("mouseover", function(){
+    document.getElementById("gametwo").style.background = "red";
+});
+
+document.addEventListener("mouseout", function(){
+    document.getElementById("gametwo").style.background = "white";
+}); */
